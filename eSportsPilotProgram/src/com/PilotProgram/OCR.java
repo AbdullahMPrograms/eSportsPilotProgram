66d588ff4279de2c79a2360078436933b9ac562f
OliverDickins <105075533+OliverDickins@users.noreply.github.com>
2022-06-19 15:20:13 -0400

    Add files via upload

---
+++ eSportsPilotProgram/src/com/PilotProgram/OCR.java
@@ -0,0 +1,36 @@
+package com.PilotProgram;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class OCR {
+	
+    
+	public static String getOCR(BufferedImage i) throws IOException {
+	Tesseract tesseract = new Tesseract();
+	String text = "";
+		try {
+
+        tesseract.setDatapath("G:/My Drive/Eclipse/Tess4J/tessdata");
+        File output = new File("screenshot.png");
+        ImageIO.write(i, "png", output);
+        System.out.println("1");
+        // the path of your tess data folder
+        // inside the extracted file
+        text = tesseract.doOCR(output);
+        // path of your image file
+       
+    }
+    catch (TesseractException e) {
+        e.printStackTrace();
+    }
+		 return text;
+}
+
+}
