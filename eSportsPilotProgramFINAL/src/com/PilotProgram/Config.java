e9bbc135f4c2ecb71206ffa84af189114d365fd3
OliverDickins <105075533+OliverDickins@users.noreply.github.com>
2022-06-23 02:40:11 -0400

    Update Config.java

--- eSportsPilotProgramFINAL/src/com/PilotProgram/Config.java
+++ eSportsPilotProgramFINAL/src/com/PilotProgram/Config.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * 
+ * @author Oliver Dickins and Abdullah Malik
+ *controls all methods that concern the cfg files and their content
+ */
 public class Config {
     static int x = 0;
     static int y = 0;
@@ -13,7 +17,10 @@ public class Config {
     static int r = 0;
     static int g = 0;
     static int b = 0;
-
+/**
+ * reads the cfg file for a given game
+ * @throws Exception
+ */
     public static void readConfig() throws Exception {
         // list that holds strings of a file
         List<String> listOfStrings = new ArrayList<String>();
@@ -44,31 +51,52 @@ public class Config {
         g = Integer.parseInt(vars[11]);
         b = Integer.parseInt(vars[13]);
     }
-
+/**
+ * returns the x value for the screenshot
+ * @return
+ */
     public static int getX() {
         return x;
     }
-
+    /**
+     * returns the y value for the screenshot
+     * @return
+     */
     public static int getY() {
         return y;
     }
-
+    /**
+     * returns the length of the screenshot
+     * @return
+     */
     public static int getLength() {
         return length;
     }
-
+    /**
+     * returns the width of the screenshot
+     * @return
+     */
     public static int getWidth() {
         return width;
     }
-
+/**
+ * returns the red value in cfg file
+ * @return
+ */
     public static int getR() {
         return r;
     }
-
+    /**
+     * returns the green value in cfg file
+     * @return
+     */
     public static int getG() {
         return g;
     }
-
+    /**
+     * returns the blue value in cfg file
+     * @return
+     */
     public static int getB() {
         return b;
     }
