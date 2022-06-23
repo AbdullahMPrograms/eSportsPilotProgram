d98265c392563e59bffb9f62dbcb5a518990cbe3
OliverDickins <105075533+OliverDickins@users.noreply.github.com>
2022-06-23 02:01:33 -0400

    Update Driver.java

--- eSportsPilotProgram/src/com/PilotProgram/Driver.java
+++ eSportsPilotProgram/src/com/PilotProgram/Driver.java
@@ -1,11 +1,21 @@
 package com.PilotProgram;
 
+import java.util.Scanner;
+
 public class Driver {
 
 	public static void main(String[] args) throws Exception {
 		Boolean loop = false;
 
-		Game.setGameInfo();
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter Game Name");
+		String gameName = in.nextLine();
+		System.out.println("Enter Game Res");
+		String gameRes = in.nextLine();
+
+		in.close();
+		Game.setGameInfo(gameName, gameRes);
 
 		Config.readConfig();
 		Screen.setCaptureRect();
