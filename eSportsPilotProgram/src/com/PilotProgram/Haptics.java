737861526bd59750d57c90c238ece97f3cd0246f
OliverDickins <105075533+OliverDickins@users.noreply.github.com>
2022-06-23 02:03:50 -0400

    Update Haptics.java

--- eSportsPilotProgram/src/com/PilotProgram/Haptics.java
+++ eSportsPilotProgram/src/com/PilotProgram/Haptics.java
@@ -5,23 +5,7 @@ import com.phidget22.Phidget;
 import com.phidget22.PhidgetException;
 
 public class Haptics {
-
-	public static void main(String[] args) throws Exception {
-		try {
-			Hub ch = new Hub();
-			ch.open(Phidget.DEFAULT_TIMEOUT);
-
-			ch.setPortPower(3, true);
-
-			Thread.sleep(300);
-
-			ch.setPortPower(3, false);
-
-			ch.close();
-		} catch (PhidgetException ex) {
-			System.out.println("Failure: " + ex);
-		}
-	}
+	static int errorCount = 0;
 
 	public static void activateHaptics() throws Exception {
 		try {
@@ -36,8 +20,14 @@ public class Haptics {
 
 			ch.close();
 		} catch (PhidgetException ex) {
-			System.out.println("Failure: " + ex);
+			errorCount++;
+			System.out.println("Error: " + ex);
 		}
+	}
+
+	public static int getErrorCount() {
+		return errorCount;
+
 	}
 
 }
